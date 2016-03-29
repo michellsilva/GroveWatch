9abb9c2d23ae9f0b1f3d171aba4bde7e506433fd
Michel Silva <8432643+michellsilva@users.noreply.github.com>
2016-03-29 14:53:00 +0000

    fix: correct cli

--- internal/provenance/scan_test.go
+++ internal/provenance/scan_test.go
@@ -9,3 +9,9 @@ import (
 
 // writeTree creates a small deterministic workspace for testing and returns its
 // root. It includes a nested directory and an ignored directory.
+func writeTree(t *testing.T) string {
+	t.Helper()
+	root := t.TempDir()
+	files := map[string]string{
+		"README.md":           "# demo\n",
+		"src/main.go":         "package main\nfunc main() {}\n",
