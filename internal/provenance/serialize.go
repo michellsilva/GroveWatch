f68036308066950ff7a7da7f533580b68b0b5411
Michel Silva <8432643+michellsilva@users.noreply.github.com>
2016-05-07 13:03:00 +0100

    refactor: tidy digest

--- internal/provenance/serialize.go
+++ internal/provenance/serialize.go
@@ -6,3 +6,7 @@ import (
 	"fmt"
 	"io"
 )
+
+// Marshal serializes a snapshot to indented, deterministic JSON. Field order is
+// fixed by the struct definition and all collections are pre-sorted during the
+// scan, so identical inputs produce byte-identical output (modulo CreatedAt).
