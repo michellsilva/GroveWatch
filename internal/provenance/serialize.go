0c45b0feb40bd4b0c27aef40f897fc2084ff6a85
Michel Silva <8432643+michellsilva@users.noreply.github.com>
2016-05-07 13:15:00 +0100

    refactor: tidy digest

--- internal/provenance/serialize.go
+++ internal/provenance/serialize.go
@@ -10,3 +10,8 @@ import (
 // Marshal serializes a snapshot to indented, deterministic JSON. Field order is
 // fixed by the struct definition and all collections are pre-sorted during the
 // scan, so identical inputs produce byte-identical output (modulo CreatedAt).
+func Marshal(snap *Snapshot) ([]byte, error) {
+	var buf bytes.Buffer
+	enc := json.NewEncoder(&buf)
+	enc.SetEscapeHTML(false)
+	enc.SetIndent("", "  ")
