a04d266de6361fc1c3d93a61d01077b70b009062
Michel Silva <8432643+michellsilva@users.noreply.github.com>
2016-12-28 15:57:00 +0000

    feat: scanner

---
+++ viewer/src/types.ts
@@ -0,0 +1,5 @@
+/**
+ * Type definitions for a grovewatch provenance snapshot. These mirror the Go
+ * model in internal/provenance/model.go exactly; keep them in sync when the
+ * schema version changes.
+ */
