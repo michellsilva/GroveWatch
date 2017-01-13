ea56bb00b66e1b7593f44ad4e387c6ff04c1af49
Michel Silva <8432643+michellsilva@users.noreply.github.com>
2017-01-13 15:21:00 +0000

    test: cover cli

---
+++ viewer/test/format.test.js
@@ -0,0 +1,7 @@
+/**
+ * Tests for the pure viewer helpers using Node's built-in test runner
+ * (node:test) and assertions (node:assert). No third-party dependencies.
+ *
+ * Run with: node --test (after compiling with tsc), or via `make viewer-test`.
+ * These import the compiled ES modules from ../dist.
+ */
