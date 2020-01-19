69cd34c80a377cfcd34be680e3c99b17d0054cad
Michel Silva <8432643+michellsilva@users.noreply.github.com>
2020-01-19 16:20:00 +0000

    style: clean up go

---
+++ scripts/serve.mjs
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+/**
+ * A tiny, zero-dependency static file server for previewing the grovewatch
+ * viewer locally. Uses only Node's standard library.
+ *
+ * Usage: node scripts/serve.mjs [port]
