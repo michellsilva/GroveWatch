60e50dc18239a3a48c9189399db08d9e11357c6c
Michel Silva <8432643+michellsilva@users.noreply.github.com>
2020-01-23 14:15:00 +0000

    docs: document provenance

--- scripts/serve.mjs
+++ scripts/serve.mjs
@@ -4,3 +4,10 @@
  * viewer locally. Uses only Node's standard library.
  *
  * Usage: node scripts/serve.mjs [port]
+ * Then open http://localhost:<port>/viewer/public/index.html
+ */
+
+import { createServer } from "node:http";
+import { readFile, stat } from "node:fs/promises";
+import { extname, join, normalize, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
