6b777442a1842cc2b5ef2ec2fc6d9e997c0ce813
Michel Silva <8432643+michellsilva@users.noreply.github.com>
2020-01-23 14:48:00 +0000

    refactor: ci

--- scripts/serve.mjs
+++ scripts/serve.mjs
@@ -18,3 +18,10 @@ const PORT = Number(process.argv[2] || process.env.PORT || 8080);
 const MIME = {
   ".html": "text/html; charset=utf-8",
   ".js": "text/javascript; charset=utf-8",
+  ".mjs": "text/javascript; charset=utf-8",
+  ".css": "text/css; charset=utf-8",
+  ".json": "application/json; charset=utf-8",
+  ".svg": "image/svg+xml",
+  ".map": "application/json; charset=utf-8",
+};
+
